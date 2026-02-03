d38d11de578211dba79e82f4363071c6801607cc
SwitchTia <oblasser.tatiana@gmail.com>
2026-02-03 11:01:40 +0100

    Feat: customerController

--- database/db.js
+++ database/db.js
@@ -5,7 +5,6 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: 3307
 })
 
 connection.connect((err) => {
