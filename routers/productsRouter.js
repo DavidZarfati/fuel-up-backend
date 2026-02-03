1b2e2fb2273cf5270e8a8d57be757171ba0f7d5a
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-02-03 16:20:58 +0100

    Creazione funzione del singolo prodotto

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,5 +4,6 @@ import productsController from "../controllers/productsController.js";
 const router = express.Router();
 
 router.get("/", productsController.indexProductsPage);
+router.get("/:slug", productsController.singleProduct)
 
 export default router;
