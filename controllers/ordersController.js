23d6f9e824bbed0e7c1ed2f3931f98200f8fb1eb
Lorenzo Minotti <cesenate90hotmail.it>
2026-02-06 18:30:12 +0100

    fix: email ricevute/inviate

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: process.env.USER,
+        user: process.env.EMAIL_USER,
         pass: process.env.APP_PASSWORD,
     },
     tls: {
