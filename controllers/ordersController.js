044ec991cf399bbe8729b67595cdaf9ed6f29d44
DavidZarfati <zarda2001@gmail.com>
2026-02-09 10:16:55 +0100

    feat:corretto user in email-user

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -329,7 +329,7 @@ function store(req, res, next) {
                                     const mailOptions = {
                                         from: {
                                             name: "FuelUp",
-                                            address: process.env.USER,
+                                            address: process.env.EMAIL_USER,
                                         },
                                         to: post.email, // email del cliente
                                         subject: "Conferma ordine FuelUp",
@@ -338,13 +338,13 @@ function store(req, res, next) {
                                     sendMail(transporter, mailOptions);
 
                                     // Invia la mail al venditore
-                                    const sellerEmail = "zarda2001@gmail.com";
+                                    const sellerEmail = process.env.EMAIL_USER;
                                     // Crea un riepilogo dei prodotti venduti
                                     const soldSummary = itemList.map(item => `- ${item.amount} x ${item.slug} (prezzo: ${item.price})`).join("\n");
                                     const mailOptionsSeller = {
                                         from: {
                                             name: "FuelUp",
-                                            address: process.env.USER,
+                                            address: process.env.EMAIL_USER,
                                         },
                                         to: sellerEmail,
                                         subject: `Hai venduto ${itemList.reduce((sum, i) => sum + i.amount, 0)} prodotti!`,
